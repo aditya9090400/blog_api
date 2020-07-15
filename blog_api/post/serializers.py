c0c0c78daa44e40b58c6cb9d666c49e0e091ba08
aditya-capital <68046684+aditya-capital@users.noreply.github.com>
2020-07-15 16:40:38 +0530

    Update serializers.py

--- blog_api/post/serializers.py
+++ blog_api/post/serializers.py
@@ -55,21 +55,3 @@ class PostDetailSerializer(serializers.ModelSerializer):
 			image_url = None 
 		return image_url
 
-'''
-blog-api
-https://www.youtube.com/watch?v=58p_mNuntd8&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=20&t=0s
-https://www.youtube.com/watch?v=CU4qv7cSnmA&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=20
-https://www.youtube.com/watch?v=vn0XVNzkJvc&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=21
-https://www.youtube.com/watch?v=DX3ekF1Xx8U&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=22
-https://www.youtube.com/watch?v=TRcVXGL1MjI&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=23
-https://www.youtube.com/watch?v=mhWvk7vyru0&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=24
-https://www.youtube.com/watch?v=B7bdoLMQrJY&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=25
-https://www.youtube.com/watch?v=fvMLvFnEBBM&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=26
-https://www.youtube.com/watch?v=5dtbbImcUCI&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=27
-https://www.youtube.com/watch?v=gNXnDlfYOqA&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=28
-https://www.youtube.com/watch?v=0jSzFUZ__k8&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=29
-https://www.youtube.com/watch?v=bDwDOatEihw&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=30
-https://www.youtube.com/watch?v=e5aL6jy4NwA&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=31
-https://www.youtube.com/watch?v=jEXQqNtjNJc&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=32
-https://www.youtube.com/watch?v=mi-AcUdGxxs&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=33
-'''
